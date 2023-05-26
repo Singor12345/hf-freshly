118a3b675d2e43a881a0c0a3500fe066ee77564b
Singor12345 <kpatchousingor1@gmail.com>
2023-05-26 18:59:21 +0100

    Maquette freshly

---
+++ Quiz-2/Freshly-Restaurant/corrigé/js/script.js
@@ -0,0 +1,16 @@
+$(document).ready(function () {
+    $(".hero").slick({
+        slidesToShow: 1,
+        dots: true,
+        autoplay: true,
+        autoplaySpeed: 3000
+    });
+});
+
+
+
+/***********************************************************************
+                            Script mixitup
+*************************************************************************/
+
+let mixer=mixitup(".img-menus")
